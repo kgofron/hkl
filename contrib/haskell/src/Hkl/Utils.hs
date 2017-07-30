3050d7e1b769938c613a9762802618993d1b0635
Picca Frédéric-Emmanuel <picca@debian.org>
2017-07-30 19:04:07 +0200

    [contrib][haskell] Hkl.Utils

--- contrib/haskell/src/Hkl/Utils.hs
+++ contrib/haskell/src/Hkl/Utils.hs
@@ -9,12 +9,9 @@ import Data.Text (Text)
 import Data.Text.IO (writeFile)
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath (takeDirectory)
--- import System.Process ( rawSystem ) -- callProcess for futur
-
-import Prelude hiding (writeFile)
 
 hasContent ∷ FilePath → Text → IO ()
 hasContent f c = do
     createDirectoryIfMissing True (takeDirectory f)
-    writeFile f c
+    Data.Text.IO.writeFile f c
     print $ "--> created : " ++ f
