cd84180caef26fb0d97cbcb7c7a5349c4b1dd502
Frédéric-Emmanuel Picca <picca@debian.org>
2018-11-08 15:11:40 +0100

    [hkl] fix compilation on amd64

--- hkl/ccan/configurator.c
+++ hkl/ccan/configurator.c
@@ -382,7 +382,7 @@ static struct test tests[] = {
 	  "	void *cq = (void *)(~((ptrdiff_t)cp));\n"
 	  "	if ((p == cp) && (q == cq))\n"
 	  "		worked = 1;\n"
-	  "	setcontext(&b);\n"
+	  "	swapcontext(&a, &b);\n"
 	  "}\n"
 	  "int main(void) {\n"
 	  "	void *ap = &worked;\n"
