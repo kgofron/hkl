4e6fb167d52a1732755146c6fd55a48950fad585
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2020-02-07 17:45:42 +0100

    [contrib/haskell] add the process subcommand

--- contrib/haskell/app/Binoculars.hs
+++ contrib/haskell/app/Binoculars.hs
@@ -12,7 +12,37 @@
 -}
 module Main where
 
+import           Options.Applicative       (CommandFields, Mod, auto, command,
+                                            execParser, fullDesc, header,
+                                            helper, hsubparser, info, long,
+                                            option, optional, progDesc,
+                                            showDefault, (<**>))
+import           Options.Applicative.Types (Parser)
+
+
 import           Hkl.Binoculars
 
+
+data Options = Process !(Maybe FilePath)
+
+processOptions :: Parser Options
+processOptions = Process <$> (optional $ option auto (long "config" <> showDefault))
+
+processCommand :: Mod CommandFields Options
+processCommand = command "process" (info processOptions (progDesc "process data's"))
+
+options :: Parser Options
+options = hsubparser processCommand
+
+run :: Options -> IO ()
+run (Process _) = process
+
 main :: IO ()
-main = process
+main = do
+  opts' <- execParser opts
+  run opts'
+    where
+      opts = info (options <**> helper)
+             ( fullDesc
+               <> progDesc "binoculars subcommand"
+               <> header "binoculars - bin your data's" )
