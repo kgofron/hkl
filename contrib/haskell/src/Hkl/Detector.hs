d3a880da2852770b4685fcf85fb8dec530263c73
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2020-11-16 16:42:51 +0100

    [contrib/haskell] use the hkl implementation of XpadFlatCorrected

--- contrib/haskell/src/Hkl/Detector.hs
+++ contrib/haskell/src/Hkl/Detector.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE ForeignFunctionInterface  #-}
 {-# LANGUAGE GADTs                     #-}
 {-# LANGUAGE QuasiQuotes               #-}
 {-# LANGUAGE StandaloneDeriving        #-}
@@ -19,11 +20,17 @@ module Hkl.Detector
 
 import           Data.Array.Repa                   (Array)
 import           Data.Array.Repa.Index             (DIM0, DIM2, DIM3, Z (..),
-                                                    ix2)
-import           Data.Array.Repa.Repr.ForeignPtr   (F)
+                                                    ix2, ix3)
+import           Data.Array.Repa.Repr.ForeignPtr   (F, fromForeignPtr)
 import           Data.Text                         (Text, unpack)
 import           Data.Vector.Storable              (Vector, fromList)
 import           Data.Word                         (Word8)
+import           Foreign.C.Types                   (CBool, CDouble (..),
+                                                    CInt (..))
+import           Foreign.ForeignPtr                (castForeignPtr,
+                                                    newForeignPtr)
+import           Foreign.Marshal.Alloc             (finalizerFree)
+import           Foreign.Ptr                       (Ptr)
 import           Numeric.Units.Dimensional.Prelude (Angle, Length, meter,
                                                     radian, (/~))
 
@@ -123,57 +130,14 @@ toPyFAIDetectorName XpadFlatCorrected = undefined
 
 getPixelsCoordinates :: Detector a DIM2 -> (Int, Int) -> Length Double -> Angle Double -> IO (Array F DIM3 Double)
 getPixelsCoordinates det (ix0, iy0) sdd detrot = do
-  extractNumpyArray =<< case det of
-                          XpadFlatCorrected -> xpadFlatCorrectedCoordinates ix0 iy0 (sdd /~ meter) (detrot /~ radian)
-                          _ -> pyfaiCoordinates (toPyFAIDetectorName det) ix0 iy0 (sdd /~ meter) (detrot /~ radian)
+  case det of
+    XpadFlatCorrected -> do
+             parr <- hkl_binoculars_detector_2d_coordinates_xpad_flat_corrected
+             hkl_binoculars_detector_2d_sixs_calibration parr 576 1154 (toEnum ix0) (toEnum iy0) (CDouble (sdd /~ meter)) (CDouble (detrot /~ radian))
+             arr <- newForeignPtr finalizerFree parr
+             return $ fromForeignPtr (ix3 3 1154 576) (castForeignPtr arr)
+    _ -> extractNumpyArray =<< pyfaiCoordinates (toPyFAIDetectorName det) ix0 iy0 (sdd /~ meter) (detrot /~ radian)
       where
-        xpadFlatCorrectedCoordinates :: Int -> Int -> Double -> Double -> IO (PyObject (Array F DIM3 Double))
-        xpadFlatCorrectedCoordinates = defVVVVO [str|
-from math import cos, sin
-from numpy import array, ascontiguousarray, copy, ones, tensordot
-from pyFAI.detectors import Detector
-
-def M(theta, u):
-    """
-    :param theta: the axis value in radian
-    :type theta: float
-    :param u: the axis vector [x, y, z]
-    :type u: [float, float, float]
-    :return: the rotation matrix
-    :rtype: numpy.ndarray (3, 3)
-    """
-    c = cos(theta)
-    one_minus_c = 1 - c
-    s = sin(theta)
-    return array([[c + u[0]**2 * one_minus_c,
-                   u[0] * u[1] * one_minus_c - u[2] * s,
-                   u[0] * u[2] * one_minus_c + u[1] * s],
-                  [u[0] * u[1] * one_minus_c + u[2] * s,
-                   c + u[1]**2 * one_minus_c,
-                   u[1] * u[2] * one_minus_c - u[0] * s],
-                  [u[0] * u[2] * one_minus_c - u[1] * s,
-                   u[1] * u[2] * one_minus_c + u[0] * s,
-                   c + u[2]**2 * one_minus_c]])
-
-def export(ix0, iy0, sdd, rot):
-        # works only for flat detector.
-        detector = Detector(130e-6, 130e-6, splineFile=None, max_shape=(1154, 576))
-        y, x, _ = detector.calc_cartesian_positions()
-        y0 = y[iy0, ix0]
-        x0 = x[iy0, ix0]
-        z = ones(x.shape) * -1 * sdd
-        # return converted to the hkl library coordinates
-        # x -> -y
-        # y -> z
-        # z -> -x
-        pixels_ = array([-z, -(x - x0), (y - y0)])
-        # rotate the detector in the hkl basis
-        P = M(rot, [1, 0, 0])
-        pixels = tensordot(P, pixels_, axes=1)
-        return copy(ascontiguousarray(pixels))
-|]
-
-
         pyfaiCoordinates :: String -> Int -> Int -> Double -> Double -> IO (PyObject (Array F DIM3 Double))
         pyfaiCoordinates = defVVVVVO [str|
 from math import cos, sin
@@ -220,36 +184,34 @@ def export(name, ix0, iy0, sdd, rot):
         return copy(ascontiguousarray(pixels))
 |]
 
+foreign import ccall unsafe
+ "hkl-binoculars.h hkl_binoculars_detector_2d_coordinates_xpad_flat_corrected"
+ hkl_binoculars_detector_2d_coordinates_xpad_flat_corrected :: IO (Ptr CDouble)
+
+foreign import ccall unsafe
+ "hkl-binoculars.h hkl_binoculars_detector_2d_sixs_calibration"
+ hkl_binoculars_detector_2d_sixs_calibration :: Ptr (CDouble) -- double *arr
+                                             -> CInt -- int width
+                                             -> CInt -- int height
+                                             -> CInt -- int ix0
+                                             -> CInt  -- int iy0
+                                             -> CDouble -- double sdd
+                                             -> CDouble -- double detrot
+                                             -> IO ()
+
+
 getDetectorDefaultMask :: Detector a DIM2 -> Maybe Text -> IO (Array F DIM2 Word8)
 getDetectorDefaultMask det mfn = do
   let fn = case mfn of
              (Just fn') -> unpack fn'
              Nothing    -> ""
-  extractNumpyArray =<< case det of
-                          XpadFlatCorrected -> xpadFlatCorrectedMask fn
-                          _                 -> pyfaiMask (toPyFAIDetectorName det) fn
+  case det of
+    XpadFlatCorrected -> do
+             arr <- newForeignPtr finalizerFree
+                 =<< hkl_binoculars_detector_2d_mask_xpad_flat_corrected
+             return $ fromForeignPtr (ix2 1154 575) (castForeignPtr arr)
+    _                 -> extractNumpyArray =<< pyfaiMask (toPyFAIDetectorName det) fn
       where
-        xpadFlatCorrectedMask :: String -> IO (PyObject (Array F DIM2 Word8))
-        xpadFlatCorrectedMask = defVO [str|
-from numpy import ascontiguousarray, bitwise_or, bool, copy, load, zeros
-import fabio
-from pyFAI.detectors import Detector
-
-def export(fnmask):
-    max_shape = (1154, 576)
-    detector = Detector(130e-6, 130e-6, splineFile=None, max_shape=max_shape)
-    mask = detector.mask
-    if mask is not None:
-        mask = mask.astype(bool)
-    else:
-        mask = zeros(max_shape).astype(bool)
-    if fnmask != "":
-        mask = bitwise_or(mask,
-                          fabio.open(fnmask).data)
-
-    return copy(ascontiguousarray(mask))
-|]
-
         pyfaiMask :: String -> String -> IO (PyObject (Array F DIM2 Word8))
         pyfaiMask = defVVO [str|
 import fabio
@@ -269,3 +231,7 @@ def export(name, fnmask):
 
     return copy(ascontiguousarray(mask))
 |]
+
+foreign import ccall unsafe
+ "hkl-binoculars.h hkl_binoculars_detector_2d_mask_xpad_flat_corrected"
+ hkl_binoculars_detector_2d_mask_xpad_flat_corrected :: IO (Ptr CBool)
