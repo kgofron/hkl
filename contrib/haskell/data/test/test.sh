7e6fa4f748cc52f8b76fa20d5083544d2c31e152
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2021-10-04 13:58:19 +0200

    [contrib/haskell] Added multiple range config

---
+++ contrib/haskell/data/test/test.sh
@@ -0,0 +1,16 @@
+binoculars-ng process config_sixs_ruche_110_m1018_32h_Qxyz_Vhkl.txt 272-285 289-316
+#binoculars process config_m1018_32h_Qxyz.txt 173-187- 190-195
+#binoculars process config_m1018_32h_Qxyz.txt 217-224
+#binoculars process config_m1018_32h_HKL.txt 103-132
+#binoculars process config_m1018_32h_HKL.txt 173-187 190-195
+#binoculars process config_m1018_32h_HKL.txt 228-257
+#binoculars process config110tige1_m1018_32h_Qxyz_Vhkl.txt 173-187 190-195
+#binoculars process config110tige2_m1018_32h_Qxyz_Vhkl.txt 40-53 55-68 71-100
+#binoculars process config210_m1018_32h_Qxyz_Vhkl.txt 103-132
+#binoculars process config210_m1018_32h_Qxyz_Vhkl.txt 134 137 140 143 146 149 152 155 158
+#binoculars process config210_m1018_32h_Qxyz_Vhkl.txt 259
+#binoculars process config210_m1018_32h_Qxyz_Vhkl.txt 262-269
+#binoculars process config_m1018_32h_Qxyz_Vhkl.txt 105-106 129 131
+#binoculars process config_m1018_32h_Qxyz.txt 105-106 129 131
+#binoculars process configV2_m1018_32h_Qxyz_Vhkl.txt 45-48 62-63 67-68
+#binoculars process config_m1018_32h_HKL.txt 33-36
