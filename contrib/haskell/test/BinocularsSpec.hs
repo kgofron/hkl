e0dd09cbcec2bb04f69d34c0e684a2caa22cdf21
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2022-03-31 10:08:56 +0200

    [contrib/haskell] simplify the test code

--- contrib/haskell/test/BinocularsSpec.hs
+++ contrib/haskell/test/BinocularsSpec.hs
@@ -8,7 +8,6 @@ module BinocularsSpec
   where
 
 
-import           Control.Monad                     (forM_)
 import           Data.Attoparsec.Text              (parseOnly)
 import           Numeric.Units.Dimensional.Prelude (meter, radian, (*~))
 import           Path                              (mkAbsDir)
@@ -59,38 +58,36 @@ spec = do
       let p = parseOnly configRangeP "120-135, 137-453"
       p `shouldBe` (Right (ConfigRange [InputRangeFromTo 120 135, InputRangeFromTo 137 453]))
 
-  describe "parseBinocularsConfig" $ do
-         forM_ [ "data/test/config_sixs_biggest.ini"
-               -- , "data/test/config_map.txt"
-               ] $ \f -> it f $ do
-           cfg :: (Either String (Config 'HklProjection)) <- getConfig =<< Just <$> getDataFileName f
-           cfg `shouldBe` (Right
-                            BinocularsConfigHkl { _binocularsConfigHklNcore = Nothing
-                                                , _binocularsConfigHklDestination = DestinationTmpl {unDestinationTmpl = "Al13Co4_facets_res_0.01_0.01_0.01_HK_4.7_5_{first}-{last}.hdf5"}
-                                                , _binocularsConfigHklOverwrite = False
-                                                , _binocularsConfigHklInputType = SixsFlyScanUhv2
-                                                , _binocularsConfigHklNexusdir = Just $(mkAbsDir "/nfs/ruche-sixs/sixs-soleil/com-sixs/2018/Run3/Corentin/Al13Co4/")
-                                                , _binocularsConfigHklTmpl = Nothing
-                                                , _binocularsConfigHklInputRange = Just (ConfigRange [InputRangeFromTo 4 5])
-                                                , _binocularsConfigHklDetector = Nothing
-                                                , _binocularsConfigHklCentralpixel = (293,141)
-                                                , _binocularsConfigHklSdd = Meter (1.1083 *~ meter)
-                                                , _binocularsConfigHklDetrot = Just (Degree (1.5707963267948966 *~ radian))
-                                                , _binocularsConfigHklAttenuationCoefficient = Just 1.825
-                                                , _binocularsConfigHklMaskmatrix = Just "/nfs/ruche-sixs/sixs-soleil/com-sixs/2018/Run3/Corentin/Al13Co4/binoculars/mask8.npy"
-                                                , _binocularsConfigHklA = Just (Angstrom (1.4452000000000001e-9 *~ meter))
-                                                , _binocularsConfigHklB = Just (Angstrom (8.158e-10 *~ meter))
-                                                , _binocularsConfigHklC = Just (Angstrom (1.2342000000000001e-9 *~ meter))
-                                                , _binocularsConfigHklAlpha = Just (Degree (1.5707963267948966 *~ radian))
-                                                , _binocularsConfigHklBeta = Just (Degree (1.5707963267948966 *~ radian))
-                                                , _binocularsConfigHklGamma = Just (Degree (1.5707963267948966 *~ radian))
-                                                , _binocularsConfigHklUx = Just (Degree ((-1.4169480479319254) *~ radian))
-                                                , _binocularsConfigHklUy = Just (Degree (0.9401030022569337 *~ radian))
-                                                , _binocularsConfigHklUz = Just (Degree ((-0.19054917649924238) *~ radian))
-                                                , _binocularsConfigHklWavelength = Nothing
-                                                , _binocularsConfigHklProjectionType = HklProjection
-                                                , _binocularsConfigHklProjectionResolution = [1.0e-2,1.0e-2,1.0e-2]
-                                                , _binocularsConfigHklProjectionLimits = Just [Limits (Just 4.45) (Just 4.95),Limits (Just 4.75) (Just 5.25),Limits Nothing Nothing]
-                                                , _binocularsConfigHklDataPath = Nothing
-                                                }
-                          )
+  describe "read and parse binoculars Configuration" $ do
+    it "hkl projection" $ do
+      cfg <- getConfig =<< Just <$> getDataFileName "data/test/config_sixs_biggest.ini"
+      cfg `shouldBe` (Right
+                      BinocularsConfigHkl { _binocularsConfigHklNcore = Nothing
+                                          , _binocularsConfigHklDestination = DestinationTmpl {unDestinationTmpl = "Al13Co4_facets_res_0.01_0.01_0.01_HK_4.7_5_{first}-{last}.hdf5"}
+                                          , _binocularsConfigHklOverwrite = False
+                                          , _binocularsConfigHklInputType = SixsFlyScanUhv2
+                                          , _binocularsConfigHklNexusdir = Just $(mkAbsDir "/nfs/ruche-sixs/sixs-soleil/com-sixs/2018/Run3/Corentin/Al13Co4/")
+                                          , _binocularsConfigHklTmpl = Nothing
+                                          , _binocularsConfigHklInputRange = Just (ConfigRange [InputRangeFromTo 4 5])
+                                          , _binocularsConfigHklDetector = Nothing
+                                          , _binocularsConfigHklCentralpixel = (293,141)
+                                          , _binocularsConfigHklSdd = Meter (1.1083 *~ meter)
+                                          , _binocularsConfigHklDetrot = Just (Degree (1.5707963267948966 *~ radian))
+                                          , _binocularsConfigHklAttenuationCoefficient = Just 1.825
+                                          , _binocularsConfigHklMaskmatrix = Just "/nfs/ruche-sixs/sixs-soleil/com-sixs/2018/Run3/Corentin/Al13Co4/binoculars/mask8.npy"
+                                          , _binocularsConfigHklA = Just (Angstrom (1.4452000000000001e-9 *~ meter))
+                                          , _binocularsConfigHklB = Just (Angstrom (8.158e-10 *~ meter))
+                                          , _binocularsConfigHklC = Just (Angstrom (1.2342000000000001e-9 *~ meter))
+                                          , _binocularsConfigHklAlpha = Just (Degree (1.5707963267948966 *~ radian))
+                                          , _binocularsConfigHklBeta = Just (Degree (1.5707963267948966 *~ radian))
+                                          , _binocularsConfigHklGamma = Just (Degree (1.5707963267948966 *~ radian))
+                                          , _binocularsConfigHklUx = Just (Degree ((-1.4169480479319254) *~ radian))
+                                          , _binocularsConfigHklUy = Just (Degree (0.9401030022569337 *~ radian))
+                                          , _binocularsConfigHklUz = Just (Degree ((-0.19054917649924238) *~ radian))
+                                          , _binocularsConfigHklWavelength = Nothing
+                                          , _binocularsConfigHklProjectionType = HklProjection
+                                          , _binocularsConfigHklProjectionResolution = [1.0e-2,1.0e-2,1.0e-2]
+                                          , _binocularsConfigHklProjectionLimits = Just [Limits (Just 4.45) (Just 4.95),Limits (Just 4.75) (Just 5.25),Limits Nothing Nothing]
+                                          , _binocularsConfigHklDataPath = Nothing
+                                          }
+                   )
