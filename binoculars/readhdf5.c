8c150b0227d8c90f1ea08eb82a81a374691df850
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2023-05-09 11:34:23 +0200

    [readhedf5] select data via hyperslab
    This version of the software read image per images all the dataset.

--- binoculars/readhdf5.c
+++ binoculars/readhdf5.c
@@ -23,26 +23,39 @@
 #include <stdlib.h>
 #include <hdf5.h>
 
+#define FILENAME "/home/picca/test-data/translation/Pt2Ag20S2_75_a2scan_deltaeiz_00912.nxs"
+#define IMAGEPATH "com/scan_data/eiger_image"
+#define DIM0 920
+#define DIM1 1065
+#define DIM2 1030
+
 int main()
 {
         size_t i;
-        const char *fn = "/home/picca/test-data/translation/Pt2Ag20S2_75_a2scan_deltaeiz_00912.nxs";
-        const char *images_path = "com/scan_data/eiger_image";
+        const char *fn = FILENAME;
+        const char *images_path = IMAGEPATH;
         hsize_t dims[] = {0, 0, 0};
-        hsize_t max_dims[] = {920, 1065, 1030};
-        uint32_t *arr = malloc(max_dims[1] * max_dims[2] * sizeof(*arr));
+        hsize_t max_dims[] = {DIM0, DIM1, DIM2};
+        hsize_t start[] = {0, 0, 0};
+        hsize_t stride[] = {1, 1, 1};
+        hsize_t count[3] = {0, DIM1, DIM2};
+        hsize_t block[3] = {0, 1, 1};;
+        uint32_t *arr;
 
         hid_t file_id;
         hid_t dataset_id;
         hid_t file_space_id;
         hid_t mem_space_id;
 
+        arr = malloc(max_dims[1] * max_dims[2] * sizeof(*arr));
         file_id = H5Fopen(fn, H5F_ACC_RDONLY, H5P_DEFAULT);
         dataset_id = H5Dopen(file_id, images_path, H5P_DEFAULT);
         file_space_id = H5Dget_space(dataset_id);
         mem_space_id = H5Screate_simple(2, &dims[1], &max_dims[1]);
 
         for(i=0; i<dims[0]; ++i){
+                start[0] = i;
+                H5Sselect_hyperslab (file_space_id, H5S_SELECT_SET, start, stride, count, block);
                 H5Dread(dataset_id, H5T_NATIVE_UINT32, mem_space_id, file_space_id, H5P_DEFAULT, arr);
         }
 
@@ -50,7 +63,6 @@ int main()
         H5Sclose(file_space_id);
         H5Dclose(dataset_id);
         H5Fclose(file_id);
-
         free(arr);
 
         return 0;
