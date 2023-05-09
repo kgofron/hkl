7f53ffd53641e85774ab238d7a4f298cfccf30ba
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2023-05-09 14:12:45 +0200

    [readhdf5] first test with real data's
    ruche 480 Mio / s

--- binoculars/readhdf5.c
+++ binoculars/readhdf5.c
@@ -23,23 +23,26 @@
 #include <stdlib.h>
 #include <hdf5.h>
 
-#define FILENAME "/home/picca/test-data/translation/Pt2Ag20S2_75_a2scan_deltaeiz_00912.nxs"
+/* #define FILENAME "/home/picca/test-data/translation/Pt2Ag20S2_75_a2scan_deltaeiz_00912.nxs" */
+#define FILENAME "/nfs/ruche-sixs/sixs-soleil/com-sixs/2021/Run3/20201559_andreazza/Pt2Ag20S2_75/Pt2Ag20S2_75_a2scan_deltaeiz_00912.nxs"
+
+
 #define IMAGEPATH "com/scan_data/eiger_image"
 #define DIM0 920
 #define DIM1 1065
 #define DIM2 1030
 
-int main()
+int main_1()
 {
         size_t i;
         const char *fn = FILENAME;
         const char *images_path = IMAGEPATH;
-        hsize_t dims[] = {0, 0, 0};
-        hsize_t max_dims[] = {DIM0, DIM1, DIM2};
+        herr_t err;
+        hsize_t dims[] = {DIM0, DIM1, DIM2};
         hsize_t start[] = {0, 0, 0};
         hsize_t stride[] = {1, 1, 1};
-        hsize_t count[3] = {0, DIM1, DIM2};
-        hsize_t block[3] = {0, 1, 1};;
+        hsize_t count[3] = {1, DIM1, DIM2};
+        hsize_t block[3] = {1, 1, 1};;
         uint32_t *arr;
 
         hid_t file_id;
@@ -47,23 +50,70 @@ int main()
         hid_t file_space_id;
         hid_t mem_space_id;
 
-        arr = malloc(max_dims[1] * max_dims[2] * sizeof(*arr));
+        arr = malloc(dims[1] * dims[2] * sizeof(*arr));
+        if(arr == NULL){
+                fprintf(stdout, "Can not allocate array\n");
+                goto exit;
+        }
         file_id = H5Fopen(fn, H5F_ACC_RDONLY, H5P_DEFAULT);
+        if(file_id < 0){
+                fprintf(stdout, "Can not open file\n");
+                goto release_arr;
+        }
         dataset_id = H5Dopen(file_id, images_path, H5P_DEFAULT);
+        if(dataset_id < 0){
+                fprintf(stdout, "can not open dataset\n");
+                goto close_file;
+        }
+
+        mem_space_id = H5Screate_simple(2, &dims[1], &dims[1]);
+        if(mem_space_id < 0){
+                fprintf(stdout, "Can not create memory dataspace\n");
+                goto close_dataset;
+        }
+
         file_space_id = H5Dget_space(dataset_id);
-        mem_space_id = H5Screate_simple(2, &dims[1], &max_dims[1]);
+        if(file_space_id<0){
+                fprintf(stdout, "can not get dataset dataspace\n");
+                goto close_mem_space;
+        }
 
         for(i=0; i<dims[0]; ++i){
                 start[0] = i;
-                H5Sselect_hyperslab (file_space_id, H5S_SELECT_SET, start, stride, count, block);
-                H5Dread(dataset_id, H5T_NATIVE_UINT32, mem_space_id, file_space_id, H5P_DEFAULT, arr);
+                err = H5Sselect_hyperslab (file_space_id, H5S_SELECT_SET, start, stride, count, block);
+                if(err<0){
+                        fprintf(stdout, "can not select hyperslab\n");
+                        goto close_mem_space;
+                }
+
+                err = H5Dread(dataset_id, H5T_NATIVE_UINT32, mem_space_id, file_space_id, H5P_DEFAULT, arr);
+                if(err<0){
+                        fprintf(stdout, "Can not read dataset content\n");
+                        goto close_mem_space;
+                }
         }
 
-        H5Dclose(mem_space_id);
         H5Sclose(file_space_id);
-        H5Dclose(dataset_id);
-        H5Fclose(file_id);
-        free(arr);
 
+close_mem_space:
+        H5Sclose(mem_space_id);
+close_dataset:
+        H5Dclose(dataset_id);
+close_file:
+        H5Fclose(file_id);
+release_arr:
+        free(arr);
+exit:
         return 0;
+}
+
+
+int main()
+{
+        size_t i;
+
+        for(i=0; i<10; ++i){
+                main_1();
+        }
+        fprintf(stdout, "octets: %ld", DIM0 * DIM1 * DIM2 * sizeof(uint32_t));
 }
