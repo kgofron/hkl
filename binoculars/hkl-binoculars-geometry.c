c3badf0779a70f1666ccf8241d144ebb01897a0e
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2024-02-04 15:46:28 +0100

    [hkl] support cglm 0.9.2 whcih just arrive in Debian unstable

--- binoculars/hkl-binoculars-geometry.c
+++ binoculars/hkl-binoculars-geometry.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with the hkl library.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2003-2023 Synchrotron SOLEIL
+ * Copyright (C) 2003-2024 Synchrotron SOLEIL
  *                         L'Orme des Merisiers Saint-Aubin
  *                         BP 48 91192 GIF-sur-YVETTE CEDEX
  *
@@ -37,12 +37,16 @@ mat4s hkl_binoculars_parameter_transformation_get(const HklParameter *self)
                         r = glms_rotate_make(self->_value, axis);
                 };
                 of(RotationWithOrigin, axis_v, pivot_v) {
-                        CGLM_ALIGN_MAT mat4s m = glms_mat4_identity();
                         CGLM_ALIGN_MAT vec3s pivot = {{pivot_v->data[0], pivot_v->data[1], pivot_v->data[2]}};
                         float angle = self->_value;
                         CGLM_ALIGN_MAT vec3s axis = {{axis_v->data[0], axis_v->data[1], axis_v->data[2]}};
 
+#if (CGLM_VERSION_MAJOR >= 0 && CGLM_VERSION_MINOR >= 9 && CGLM_VERSION_PATH > 1)
+                        CGLM_ALIGN_MAT mat4s m = glms_mat4_identity();
                         r = glms_rotate_atm(m, pivot, angle, axis);
+#else
+                        r = glms_rotate_atm(pivot, angle, axis);
+#endif
                 };
                 of(Translation, v_v) {
                         CGLM_ALIGN_MAT vec3s v = {{v_v->data[0], v_v->data[1], v_v->data[2]}};
